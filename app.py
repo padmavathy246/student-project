f5cd9fac1d676eeeced5170f5d6b83f737f0b6ba
padmavathy246 <padmarose24@gmail.com>
2026-03-24 19:51:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
+# Create app
 app = Flask(__name__)
 
 # Load model
-model = pickle.load(open("model.pkl", "rb"))
+model = pickle.load(open('model.pkl', 'rb'))
 
+# Home page
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
+# Prediction
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        study = float(request.form['study'])
-        attendance = float(request.form['attendance'])
-        marks = float(request.form['marks'])
+    amount = float(request.form['amount'])
+    time = float(request.form['time'])
 
-        data = np.array([[study, attendance, marks]])
-        prediction = model.predict(data)
+    # Prediction
+    prediction = model.predict([[amount, time]])
 
-        output = "Pass" if prediction[0] == 1 else "Fail"
+    if prediction[0] == -1:
+        result = "⚠️ Fraud Detected"
+    else:
+        result = "✅ Normal Transaction"
 
-        return render_template("index.html", prediction_text=output)
-    except:
-        return "Error occurred. Check input!"
+    return render_template('index.html', prediction_text=result)
 
+# Run app
 if __name__ == "__main__":
     app.run(debug=True)
